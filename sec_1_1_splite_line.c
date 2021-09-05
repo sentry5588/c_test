470e20ddb9d65471cb5eaea27ee735ffb52c8c6e
sentry5588 <49176772+sentry5588@users.noreply.github.com>
2021-09-05 15:20:29 -0400

    un_matched function parameter names

--- sec_1_1_splite_line.c
+++ sec_1_1_splite_line.c
@@ -19,9 +19,20 @@ void copy_file(void)
         putchar(c);
 }
 
+/* K&R function prototype, parameter mis-match */
+/* Note that the function prototype and function definition has 
+    different parameter names, i.e., x vs a, and y vs b */
+void un_match_param(int x, double y);
+
+void un_match_param(int a, double b)
+{
+    printf("[%s] param: a = %d, b = %f\n", __FUNCTION__, a, b);
+}
+
 int main(void)
 {
     //    break_lines();
-    copy_file();
+    // copy_file();
+    un_match_param(2, 3.3);
     return 0;
 }
